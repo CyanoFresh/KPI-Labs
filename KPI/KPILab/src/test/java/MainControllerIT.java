d74b39e8ebf7d3953f457994e7f90130c5bdc4e8
CyanoFresh <cyanofresh@gmail.com>
2021-03-21 15:05:06 +0200

    added test

--- KPI/KPILab/src/test/java/MainControllerIT.java
+++ KPI/KPILab/src/test/java/MainControllerIT.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class MainControllerIT {
@@ -11,12 +12,24 @@ public class MainControllerIT {
         when(view.readInput()).thenReturn("1 2 3 4");
         when(polygon.calcS()).thenReturn(1.0);
 
-        var controller = new MainController(polygon,view);
+        var controller = new MainController(polygon, view);
         controller.run();
 
         verify(polygon).setX(1);
         verify(polygon).setY(2);
         verify(polygon).setVertexCount(3);
         verify(polygon).setSide(4);
+    }
+
+    @Test
+    void mockIncorrectInput() throws Throwable {
+        var polygon = mock(Polygon.class);
+        var view = mock(MainView.class);
+
+        when(view.readInput()).thenReturn("1 2 3");
+
+        var controller = new MainController(polygon, view);
+
+        assertThrows(Exception.class, controller::run);
     }
 }

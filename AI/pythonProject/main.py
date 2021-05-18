c05e9a7be4efbb2ef06051425331052337239b57
CyanoFresh <cyanofresh@gmail.com>
2021-05-19 02:16:49 +0300

    ai lab4

---
+++ AI/pythonProject/main.py
@@ -0,0 +1,55 @@
+import numpy as np
+from operator import itemgetter
+import random
+
+gamma = 0.8
+N = 3 * 3
+states = np.arange(N)
+finish_state = 0
+Q = np.zeros((N, N))
+R = [
+    [100, 0, -1, 0, 0, -1, -1, -1, -1],
+    [100, -1, 0, 0, 0, 0, -1, -1, -1],
+    [-1, 0, -1, -1, 0, 0, -1, -1, -1],
+    [100, 0, -1, -1, 0, -1, 0, 0, -1],
+    [100, 0, 0, 0, -1, 0, 0, 0, 0],
+    [-1, 0, 0, -1, 0, -1, -1, 0, 0],
+    [-1, -1, -1, 0, 0, -1, -1, 0, -1],
+    [-1, -1, -1, 0, 0, 0, 0, -1, 0],
+    [-1, -1, -1, -1, 0, 0, -1, 0, -1],
+]
+
+
+def get_next_action(state, matrix):
+    next_actions = [(x, i) for i, x in enumerate(matrix[state]) if x != -1]
+    actions_sum = sum([x[0] for x in next_actions])
+
+    if actions_sum == 0:
+        return random.choice(next_actions)[1]
+
+    max_action = max(next_actions, key=itemgetter(0))
+    return max_action[1]
+
+
+for i in range(50):
+    state = np.random.choice(states)
+
+    while True:
+        next_state = get_next_action(state, R)
+
+        Q[state][next_state] = int(R[state][next_state] + gamma * max(Q[next_state]))
+
+        if state == finish_state:
+            break
+
+        state = next_state
+
+print(Q)
+
+# Test
+state = 5
+
+while state != finish_state:
+    next_action = get_next_action(state, Q)
+    print(state, '->', next_action)
+    state = next_action

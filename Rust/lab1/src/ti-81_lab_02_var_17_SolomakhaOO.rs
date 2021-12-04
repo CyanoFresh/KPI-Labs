ad18e9a8940570105d2d7c570c8fb34ffcf59527
CyanoFresh <cyanofresh@gmail.com>
2021-12-04 23:31:41 +0200

    lab 2 task 2 and 3 done

--- Rust/lab1/src/ti-81_lab_02_var_17_SolomakhaOO.rs
+++ Rust/lab1/src/ti-81_lab_02_var_17_SolomakhaOO.rs
@@ -2,14 +2,18 @@
 
 /*
 
-204
+692 e
 
 Дана действительная квадратная матрица порядка n. Найти наибольшее из значений элементов, расположенных
-в заштрихованой части матрицы
+в заштрихованной части матрицы:
 _____
 |\ /|
 |/ \|
 ^^^^^
+
+1 0 1
+1 1 1
+1 0 1
 
 */
 fn task692e(a: &Vec<Vec<f64>>) -> f64 {
@@ -28,7 +32,7 @@ fn task692e(a: &Vec<Vec<f64>>) -> f64 {
             }
         }
 
-        if n%2==0 {
+        if n % 2 == 0 {
             if col < center - 1 {
                 k += 1;
             } else if col == center {
@@ -75,6 +79,130 @@ mod tests692e {
         let result = task692e(&a);
 
         assert_eq!(16., result);
+    }
+}
+
+/*
+
+694 г
+
+Получить квадратную матрицу порядка n
+
+*/
+fn task694g(n: usize) -> Vec<Vec<i32>> {
+    let mut matrix: Vec<Vec<i32>> = vec![vec![0; n]; n];
+
+    matrix = matrix
+        .iter()
+        .enumerate()
+        .map(|(i, row)| row.iter().enumerate().map(|(j, &el)| return if i == j { ((i + 1) * (i + 2)) as i32 } else { el }).collect())
+        .collect();
+
+    println!("{:?}", matrix);
+
+    matrix
+}
+
+#[cfg(test)]
+mod tests694g {
+    use super::*;
+
+    #[test]
+    fn n2() {
+        let n = 2;
+
+        let expected = vec![
+            vec![1 * 2, 0],
+            vec![0, 2 * 3],
+        ];
+
+        let result = task694g(n);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn n5() {
+        let n = 5;
+
+        let expected = vec![
+            vec![1 * 2, 0, 0, 0, 0],
+            vec![0, 2 * 3, 0, 0, 0],
+            vec![0, 0, 3 * 4, 0, 0],
+            vec![0, 0, 0, 4 * 5, 0],
+            vec![0, 0, 0, 0, 5 * 6],
+        ];
+
+        let result = task694g(n);
+
+        assert_eq!(expected, result);
+    }
+}
+
+/*
+
+580
+
+Дано натуральное число n (n <= 99). Получить
+все способы выплаты суммы n с помощью монет достоинством 1, 5, 10 и 20 коп.
+
+*/
+fn task580(n: usize) -> Vec<Vec<usize>> {
+    assert!(n > 0);
+    assert!(n <= 99);
+
+    let mut result = vec![];
+
+    for k20 in 0..=n / 20 {
+        for k10 in 0..=(n - 20 * k20) / 10 {
+            for k5 in 0..=(n - 20 * k20 - 10 * k10) / 5 {
+                let k1 = n - 20 * k20 - 10 * k10 - 5 * k5;
+
+                result.push(vec![k20, k10, k5, k1]);
+            }
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests580 {
+    use super::*;
+
+    #[test]
+    fn n4() {
+        let n = 4;
+
+        let expected = vec![
+            vec![0, 0, 0, 4],
+        ];
+
+        let result = task580(n);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn n20() {
+        let n = 20;
+
+        let expected = vec![
+            vec![0, 0, 0, 20],
+            vec![0, 0, 1, 15],
+            vec![0, 0, 2, 10],
+            vec![0, 0, 3, 5],
+            vec![0, 0, 4, 0],
+            vec![0, 1, 0, 10],
+            vec![0, 1, 1, 5],
+            vec![0, 1, 2, 0],
+            vec![0, 2, 0, 0],
+            vec![1, 0, 0, 0],
+        ];
+
+        let result = task580(n);
+
+        assert_eq!(expected, result);
     }
 }
 
